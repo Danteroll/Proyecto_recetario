cb092a4228a86cb9716d2ae79f6ec35832dfbd46
Danteroll <79463182+Danteroll@users.noreply.github.com>
2023-02-24 11:02:04 -0600

    formulario_prueba0.2

--- formulario.php
+++ formulario.php
@@ -8,27 +8,5 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
     <link rel="stylesheet" href="formulario.css">
-	<script type="text/javascript" src="validacion.js"></script>
 </head>
 <body>
-<section id="contacto">
-<form action="registro.php" method="post" id="registroForm">
-			<label for="nombre">Nombre:</label>
-			<input type="text" id="nombre" name="nombre" required>
-
-			<label for="email">Email:</label>
-			<input type="email" id="email" name="email" required>
-
-			<label for="password">Contraseña:</label>
-			<input type="password" id="password" name="password" required>
-
-			<label for="confirm_password">Confirmar Contraseña:</label>
-			<input type="password" id="confirm_password" name="confirm_password" required>
-
-			<input type="submit" value="Registrarse">
-
-			<p>¿Ya tienes una cuenta? <a href="#">Iniciar sesión</a></p>
-		</form>
-</section>
-</body>
-</html>
