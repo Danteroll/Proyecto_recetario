0b3498b94f8bda7a52c653c08378e39d406eb704
Danteroll <79463182+Danteroll@users.noreply.github.com>
2023-02-26 20:05:43 -0600

    formulario_0.6

--- formulario.php
+++ formulario.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>formulario</title>
     <link rel="stylesheet" href="formulario.css">
-</head>
 <body>
     <main>
-        <div class="container__todo">
+
+        <div class="contenedor__todo">
             <div class="caja__trasera">
                 <div class="caja__trasera-login">
                     <h3>¿Ya tienes una cuenta?</h3>
-                    <p>Incia sesión para entrar en la página</p>
+                    <p>Inicia sesión para entrar en la página</p>
                     <button id="btn__iniciar-sesion">Iniciar Sesión</button>
                 </div>
                 <div class="caja__trasera-register">
@@ -22,26 +22,29 @@
                     <button id="btn__registrarse">Regístrarse</button>
                 </div>
             </div>
-            <!--Formulario de login y registro-->
-            <div class="container__login-register">
+
+            <!--Formulario de Login y registro-->
+            <div class="contenedor__login-register">
                 <!--Login-->
                 <form action="" class="formulario__login">
-                    <h2>Inciar Sesión</h2>
+                    <h2>Iniciar Sesión</h2>
                     <input type="text" placeholder="Correo Electronico">
                     <input type="password" placeholder="Contraseña">
                     <button>Entrar</button>
                 </form>
+
                 <!--Register-->
-                <form action="" class="formulario__regiter">
+                <form action="php/registro_usuario_be.php" method="POST" class="formulario__register">
                     <h2>Regístrarse</h2>
-                    <input type="text" placeholder="Nombre Completo">
-                    <input type="text" placeholder="Correo Electronico">
-                    <input type="text" placeholder="Usuario">
-                    <input type="password" placeholder="Contraseña">
+                    <input type="text" placeholder="Nombre completo" name="nombre_completo">
+                    <input type="text" placeholder="Correo Electronico" name="correo">
+                    <input type="text" placeholder="Usuario" name="usuario">
+                    <input type="password" placeholder="Contraseña" name="contrasena">
                     <button>Regístrarse</button>
                 </form>
             </div>
         </div>
     </main>
+    <script src="formulario.js"></script>
 </body>
 </html>
